8d97ba10ae57733427707c2438db0015d4f5013a
NB-Matlala <njabulo_matlala@yahoo.com>
2022-08-10 18:28:23 +0200

    Edited forked code
    Simple compound interest program

--- CompoundInterest.py
+++ CompoundInterest.py
@@ -1,30 +1,27 @@
+import math
 
-print("Welcome to the Zeze investment portal!")
+print("Welcome to the NB investment portal!")
+print("Please input the info required to begin the process\n\n")
 
-print("Please input the info required to begin the process")
+age = int(input("How old are you? "))
 
-age = int(input("How old are you? - "))
+income = int(input("How much are you paid in ZAR? "))
+principal = int(input("How much are you willing to invest(Investments start from ZAR100)?"))
 
-print(age)
+while (principal <100):
+    print("\nMinimum investment is 100ZAR.")
+    principal = int(input("How much are you willing to invest(Investments start from ZAR100)?"))
+else:
+    time = int(input('How long do you want to invest(In years)? '))
 
-income = int(input("How much are you paid in GHS? - "))
+    print('Interest rate is 16% per annum')
+    interestRate = 0.16
+finalAmount = principal* 1 + pow(interestRate,time)
 
-incomeVal = int(print(income))
-
-principal = int(input("How much are you willing to invest?(Investments start from GHS100 and above) - "))
-
-principalVal = (print(principal))
-
-time = int(input('How long do you want to invest?(In years)  - '))
-
-timeVal = (print(time))
-
-print('Interest rate is 16% per annum')
-interestRate = float(0.16)
-
-finalAmount = float(principalVal * (1 + (interestRate)) ^ (time))
-
-print(finalAmount)
-
-print(f"This how much you'll be making at the end of the time period - {finalAmount}")
-	
+print("\n=========================================")
+print(f"Investor Age: {age}")
+print("Income: " + "R{:,.2f}".format(income))
+print(f"Investment duration: {time} years")
+print(f"Interest Rate: {interestRate * 100}%")
+print("\nNet Profit of Investment: "+"R{:,.2f}".format(finalAmount))
+print("=========================================")	
